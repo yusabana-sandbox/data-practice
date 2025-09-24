70ccd9f90b5a55c72c2c947539cd7aa841d47a7a
Yuji Takaesu <taka.esu.user@gmail.com>
2025-09-25 00:42:19 +0900

    Fix indents

--- dbt_introduction/models/materialized_dwh/employee_names.sql
+++ dbt_introduction/models/materialized_dwh/employee_names.sql
@@ -1,19 +1,19 @@
 {{
-    config(
-        materialized='incremental',
-		alias='employee_names',
-    )
+  config(
+    materialized='incremental',
+    alias='employee_names',
+  )
 }}
 
 select
-	"employee_id",
-	concat("first_name", ' ', "last_name") as full_name
+  "employee_id",
+  concat("first_name", ' ', "last_name") as full_name
 from
-	"dbt_training"."raw"."employees"
+  "dbt_training"."raw"."employees"
 where first_name = '{{ var("hoge_type", "taro") }}'
 
 {% if is_incremental() %}
 
-	and "employee_id" not in (select "employee_id" from {{ this }})
+  and "employee_id" not in (select "employee_id" from {{ this }})
 
 {% endif %}
